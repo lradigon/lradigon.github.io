47ba5bdd54f0f4a572ea2416ee42b95ddfeb0d45
lradigon <leo.radigon@epitech.eu>
2025-03-09 17:43:52 +0100

    finish

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-let index = 1;
-
-document.querySelector('.carousel-arrow-left').addEventListener('click', function() {
-    const image = document.querySelector('.carousel-images img');
-    index -= 1;
-    if (index < 1)
-        index = 4;
-    image.src = `./assets/slide-${index}.jpg`    
-});
-
-document.querySelector('.carousel-arrow-right').addEventListener('click', function() {
-    const image = document.querySelector('.carousel-images img');
-    index += 1;
-    if (index > 4)
-        index = 1;
-    image.src = `./assets/slide-${index}.jpg`    
-});
+let index=1;document.querySelector('.carousel-arrow-left').addEventListener('click',function(){const image=document.querySelector('.carousel-images img');index-=1;if(index<1)
+    index=4;image.src=`./assets/slide-${index}.jpg`});document.querySelector('.carousel-arrow-right').addEventListener('click',function(){const image=document.querySelector('.carousel-images img');index+=1;if(index>4)
+    index=1;image.src=`./assets/slide-${index}.jpg`})
