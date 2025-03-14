8b33d8354d257286adfff645c5499fdc0f0f69a7
lradigon <72014988+lradigon@users.noreply.github.com>
2025-03-14 21:17:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 let index=1;document.querySelector('.carousel-arrow-left').addEventListener('click',function(){const image=document.querySelector('.carousel-images img');index-=1;if(index<1)
-    index=4;image.src=`./assets/slide-${index}.jpg`});document.querySelector('.carousel-arrow-right').addEventListener('click',function(){const image=document.querySelector('.carousel-images img');index+=1;if(index>4)
-    index=1;image.src=`./assets/slide-${index}.jpg`})
+    index=4;image.src=`./assets/slide-${index}.webp`});document.querySelector('.carousel-arrow-right').addEventListener('click',function(){const image=document.querySelector('.carousel-images img');index+=1;if(index>4)
+    index=1;image.src=`./assets/slide-${index}.webp`})
